ce39e2cce5ec0757b4401678699c3dce5bd12d61
Niv Sardi <xaiki@evilgiggle.com>
2016-06-30 14:27:02 -0300

    call flatpak with exec
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/kalite-flatpak-wrapper.sh
+++ src/kalite-flatpak-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Running KA-Lite via flatpak with pid: ${LISTEN_PID}"
-flatpak run org.learningequality.kalite \
+exec flatpak run org.learningequality.kalite \
         --env="LISTEN_PID=${LISTEN_PID}" \
         --command="/app/bin/kalite start"
